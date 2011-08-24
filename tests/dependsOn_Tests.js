e1e1ab96ef61b54639245a287cbae0a47f24fdc9
James Eggers <james.r.eggers@gmail.com>
2011-08-23 22:13:41 -0500

    Initial code written

---
+++ tests/dependsOn_Tests.js
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2011 James Eggers, http://www.jamesreggers.com
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var fixture = new TestCase("Unit Tests for dependsOn");
+
+fixture.prototype["test Verify the method is defined."] = function(){
+	assertNotUndefined("dependsOn should be defined if loaded.", window.dependsOn);
+	assertTypeOf("dependsOn should be defined as a function.", "function", window.dependsOn);
+}
+
+fixture.prototype["test Verify the dependency exists for a provided object. (happy path)"] = function(){
+	var config = { property1: "test"};
+	
+	assertTrue("The config should contain a property1 member.", dependsOn("property1", config));
+}
+
+fixture.prototype["test Throw an error if the dependency does not exist for a provided object. (sad path)"] = function(){
+	var config = { property1: "test"};
+	
+	assertException("The config should be missing a property2 member.", dependsOn("property2", config), "TypeException"));
+}
+
+fixture.prototype["test Verify the dependency exists using default scope. (happy path)"] = function(){
+	window.prototype.property1 =  "test";
+	
+	assertTrue("The config should contain a property1 member.", dependsOn("property1"));
+}
+
+fixture.prototype["test Throw an error if the dependency does not exist for a provided object. (sad path)"] = function(){
+	
+	assertException("The config should be missing a property2 member.", dependsOn("property2"), "TypeException"));
+}
